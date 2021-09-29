83fc801b0bcc944ae64a19087ab3c6727eb6e80d
sakshi <tiwari2000sakshi@gmail.com>
2021-09-29 17:15:21 +0530

    made the home page

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAF6evbqPoIu8QSNjcDKUHxItH0a40arVc",
+  authDomain: "clone-70df0.firebaseapp.com",
+  projectId: "clone-70df0",
+  storageBucket: "clone-70df0.appspot.com",
+  messagingSenderId: "1052283840781",
+  appId: "1:1052283840781:web:b4f9f48580f26ba0362844"
+};
